3cad2f36c4831a5ea146a9810affedf82a287feb
antadlp <ant.adlp1986@gmail.com>
2016-06-19 13:34:41 -0500

    thoug life

--- ex5.py
+++ ex5.py
@@ -1,5 +1,10 @@
 my_name = 'Zed A. Shaw'
 my_age = 35 #not a lie
 my_height = 74 #inches
-my_wight = 180 # lbs
+my_weight = 180 # lbs
 my_eyes 'Blue'
+my_teeth='white'
+my_hair='Brown'
+
+print "Let's talk about %s" % my_name
+print "He's %d inches tall." % my_height
---
+++ ex5.py~
@@ -0,0 +1,5 @@
+my_name = 'Zed A. Shaw'
+my_age = 35 #not a lie
+my_height = 74 #inches
+my_wight = 180 # lbs
+my_eyes 'Blue'
