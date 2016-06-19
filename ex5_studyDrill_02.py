b007cc62884cec0542eefecdd937c04fe8eaa5d5
antadlp <ant.adlp1986@gmail.com>
2016-06-19 14:09:45 -0500

    thoug life

---
+++ ex5_studyDrill_02.py
@@ -0,0 +1,15 @@
+#2. Try to write some variables that convert the inches and pounds to
+#centimeters and kilograms. Do not just type in the measurements. Work out the
+#math in Python
+
+#input centimetros And pounds
+inCm = 2.54
+inPounds = 30
+
+print "Las pulgadas que quieres convertir son %f", inCm
+print "que son %f centimetros", inCm*2.54
+print "\n"
+print "Y las libras que quieres convertir son %f", inPounds
+print "que son %f kilogramos", inPounds*454
+
+
