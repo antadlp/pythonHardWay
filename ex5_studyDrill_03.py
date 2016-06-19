14f18fdae60d8f947b8e3af641517e03ea27a446
antadlp <ant.adlp1986@gmail.com>
2016-06-19 15:08:09 -0500

    thoug life

--- ex5_studyDrill_03.py
+++ ex5_studyDrill_03.py
@@ -10,12 +10,15 @@ print "%e : Floating point exponential format (lowercase)"
 print "%E : Floating point exponential format (uppercase)"
 print "%f : Floating point decimal format"
 print "%F : Floating point decimal format"
-print "%g : Same as 'e' if exponent is grater than -4 or less than precision,
-'f' otherwise"
-print "%G : Same as 'e' if exponent is grater than -4 or less than precision,
+print "%g : Same as 'e' if exponent is grater than -4 or less than precision \
+'f' otherwise" 
+print "%G : Same as 'e' if exponent is grater than -4 or less than precision, \
 'F' otherwise"
 print "%c : Single characters (accepts integer or single character string)"
 print "%r : String (converts any python object using repr())"
+print "%s : String (converts any python objetc using str())"
+print "%  : No arguments is converted, results in a '%' character in the \
+result"
 
 
 
